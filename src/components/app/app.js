3b40128889a525b79326348e14b4b54088d12d2a
Sergey Tyurikov <project-present@yandex.ru>
2023-02-12 06:01:47 +0700

    New features

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import {Component} from "react";
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,20 +8,59 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-  return (
-    <div className="app">
-        <AppInfo />
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {
+                    name: 'Doctor',
+                    salary: 800,
+                    increase: false,
+                    id: 1,
+                },
+                {
+                    name: 'Sergey',
+                    salary: 3000,
+                    increase: true,
+                    id: 2,
+                },
+                {
+                    name: 'Carl',
+                    salary: 5000,
+                    increase: false,
+                    id: 3,
+                },
+            ]
+        }
+    }
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    render() {
+
+        return (
+            <div className="app">
+                <AppInfo/>
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+
+                <EmployeesList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
+            </div>
+        );
+    }
 }
 
 export default App;
